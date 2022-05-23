9d66deb90ea3fa8574f762562e6ea707c2d278f1
Al-r40 <95902927+Al-r40@users.noreply.github.com>
2022-05-23 22:37:11 +0400

    fix: task#5 is done!

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,19 +1,37 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readdir(path.join(__dirname, './styles'), (err, files) => {
-    files.forEach(file => {
-        if (path.extname(file).slice(1) == 'css') {
-            fs.readFile(path.join(__dirname, `./styles/${file}`), 'utf8', (err) => {
-                if(err) throw err;
-                fs.appendFile(path.join(__dirname, './project-dist/bundle.css'), `@import url('../styles/${file}');\n`, (err) => {
+function add() {
+    fs.readdir(path.join(__dirname, './styles'), (err, files) => {
+        files.forEach(file => {
+            if (path.extname(file).slice(1) == 'css') {
+                fs.readFile(path.join(__dirname, `./styles/${file}`), 'utf8', (err) => {
                     if(err) throw err;
+                    fs.appendFile(path.join(__dirname, './project-dist/bundle.css'), `@import url('../styles/${file}');\n`, (err) => {
+                        if(err) throw err;
+                    });
                 });
-            });
-        }
+            }
+        });
     });
-});
+    
+    fs.open(path.join(__dirname, './project-dist/bundle.css'), 'a', (err) => {
+        if(err) throw err;
+    });
+}
 
-fs.open(path.join(__dirname, './project-dist/bundle.css'), 'a', (err) => {
-    if(err) throw err;
+function del() {
+    fs.writeFile(path.join(__dirname, './project-dist/bundle.css'), ' ', (err) => {
+        if(err) throw err;
+    });
+}
+
+
+fs.access(path.join(__dirname, './project-dist/bundle.css'), function(error){
+    if (error) {
+        add();
+    } else {
+        del();
+        add();
+    }
 });
